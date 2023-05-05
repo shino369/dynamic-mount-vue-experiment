801cf7bba7f58ab80c18131512ffe1e39c9e329a
SHINO <48188018+shino369@users.noreply.github.com>
2023-05-05 23:38:02 +0900

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
